1a7faa1a530673ac4de4c65a178f11cb88321216
Gabriel LePoudre <gjl774@mail.usask.ca>
2021-12-03 15:27:15 -0600

    Made proper get requests actually gets

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.post('/staff_del', (req, res) => {
 
     res.send(`OK, deleted ${id} from staff`) // or, depending on implementation, this can be a list of registered staff
 })
-app.post('/staff_view', (req, res) => {
+app.get('/staff_view', (req, res) => {
     // var name = req.body.name;
     //TODO: find customer id by name in SQL
     //      get details from that customer
@@ -216,7 +216,7 @@ app.post('/customer_report', (req, res) => {
     res.send(`OK, added ${lName}'s report`) // or, depending on implementation, this can be a list of the registered customers (or of that specific customers reports)
 })
 
-app.post('/get_reports', (req, res) => {
+app.get('/get_reports', (req, res) => {
     let id = req.body.id;
 
     connection.query({
@@ -234,7 +234,7 @@ app.post('/get_reports', (req, res) => {
 
 })
 
-app.post('/customer_view', (req, res) => {
+app.get('/customer_view', (req, res) => {
     // var name = req.body.name;
     //TODO: find customer id by name in SQL
     //      get details from that customer
